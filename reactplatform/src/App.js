a60ae8fcc731cf4012c3e9f336bd34da81c43d40
josiehpersson <josephine.h.persson@outlook.com>
2020-04-20 09:49:37 +0200

    #1

--- reactplatform/src/App.js
+++ reactplatform/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import './Komponenter/Boll.css';
+import Frostatglas from './Komponenter/Frostatglas';
+import Boll from './Komponenter/Boll';
 
 function App() {
   return (
     <div className="App">
-
+      <Frostatglas />
+      <Boll bolltyp='blåboll' />
+      <Boll bolltyp='grönboll' />
+      <Boll bolltyp='gulboll' />
     </div>
   );
 }
---
+++ reactplatform/src/Komponenter/Boll.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Boll.css';
+
+function Boll(props) {
+
+  return <div className={props.bolltyp}></div>;
+}
+
+export default Boll;
