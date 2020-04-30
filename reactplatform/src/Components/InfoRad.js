cf0442b0167216fbed7ff94e17c0b0ad544e9934
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-04-30 10:48:14 +0200

    Create InfoRad.js

---
+++ reactplatform/src/Components/InfoRad.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+function InfoRad(){
+  return(
+    <div className="InfoRad">
+      <div className="top">
+        <h6>KONSULT</h6>
+        <h6>ÅR</h6>
+        <h6>MÅNAD</h6>
+        <h6>TIMMAR</h6>
+        <h6>STATUS</h6>
+      </div>
+      <div className="bottom">
+        <h6>PEDRO PERDOSSON</h6>
+        <h6>2020</h6>
+        <h6>JUNI</h6>
+        <h6>240</h6>
+        <h6>S</h6>
+      </div>
+    </div> 
+  )
+}
+
+
+
+export default InfoRad
