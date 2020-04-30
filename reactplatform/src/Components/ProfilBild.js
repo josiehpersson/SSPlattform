043c6d1b2c8b3d717e92eb83cdf2be065a354b97
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-04-30 17:35:34 +0200

    Create ProfilBild.js

---
+++ reactplatform/src/Components/ProfilBild.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function ProfilBild(props){
+    
+    return(
+       <div className="ProfilBild">
+           <img src={Image.props} alt="Profile pic" className="Logotypeboll" />
+       </div>     
+    )
+}
+
+
+export default ProfilBild
