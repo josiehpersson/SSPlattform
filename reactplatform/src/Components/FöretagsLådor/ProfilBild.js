1e4a0e405b1cdd8a9a047f9442a0c331dcccacf3
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-05-05 16:29:54 +0200

    Create ProfilBild.js

---
+++ reactplatform/src/Components/FöretagsLådor/ProfilBild.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfilBild(props) {
+  return (
+    <div className="ProfilBild">
+      <img src={props.img} alt={props.alt} className="Logotypeboll" />
+    </div>
+  );
+}
+
+export default ProfilBild;
