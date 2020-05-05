ed0d8ecb6786766932fec4f1795694403a84e22f
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-05-05 16:30:27 +0200

    Update Företagslåda.js

--- reactplatform/src/Components/FöretagsLådor/Företagslåda.js
+++ reactplatform/src/Components/FöretagsLådor/Företagslåda.js
@@ -1,4 +1,5 @@
 import React from "react";
+import React from 'react';
 import ProfilBild from './ProfilBild';
 import PrimaryButton from '../PrimaryButton';
 
@@ -21,8 +22,23 @@ function Företagslåda(props){
                 </div>
             </div>
             
+function Företagslåda(props) {
+  return (
+    <div>
+      <div className="boxNav">
+        <ProfilBild src={props.img} alt={props.alt} />
+      </div>
+      <div className="boxBody">
+        <div className="FöretagsInfo">
+          <h4>{props.företagsNamn}</h4>
+          <h6>{props.stadNamn}</h6>
+          <h5>{props.företagstyp}</h5>
+          <PrimaryButton text="Info" />
         </div>
     )
+      </div>
+    </div>
+  );
 }
 
 export default Företagslåda 
