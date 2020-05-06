2dab516c7d87026b1322aa202d2c25d0e00f0e9c
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-05-06 11:32:00 +0200

    Update Företagslåda.js

--- reactplatform/src/Components/FöretagsLådor/Företagslåda.js
+++ reactplatform/src/Components/FöretagsLådor/Företagslåda.js
@@ -1,23 +1,24 @@
-import React from "react";
+import React from 'react';
 import ProfilBild from './ProfilBild';
 import PrimaryButton from '../PrimaryButton';
+import PopupProfile from './PopupProfile';
 
-function Företagslåda(props){
-    return(
-        <div>
-             
-            <div className="boxNav">
-            </div>
-            <div className="boxBody">
-                <div className="FöretagsInfo">
-                    <h4>{props.företagsNamn}</h4>
-                    <h6>{props.stadNamn}</h6>
-                    <h5>{props.företagstyp}</h5>
-                    <PrimaryButton 
-                     text="Info" />
-                </div>
-            </div>
+function Företagslåda(props) {
+  return (
+    <div className="FöretagsLådor">
+      <div className="boxNav">
+        <ProfilBild />
+      </div>
+      <div className="boxBody">
+        <div className="FöretagsInfo">
+          <h4>{props.företagsNamn}</h4>
+          <h6>{props.stadNamn}</h6>
+          <h5>{props.företagstyp}</h5>
+          <PopupProfile />
+          <PrimaryButton text="Info" />
         </div>
+      </div>
+    </div>
   );
 }
 
