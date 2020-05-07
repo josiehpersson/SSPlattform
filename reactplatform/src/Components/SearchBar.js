01f9bc32f23740970c9ff602c6d04c73053c2c1e
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-05-07 09:55:07 +0200

    Create SearchBar.js

---
+++ reactplatform/src/Components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <form class="form-inline active-cyan-3 active-cyan-4">
+        <i class="fas fa-search" aria-hidden="true"></i>
+        <input
+          class="form-control form-control-sm w-40"
+          type="text"
+          placeholder="Sök"
+          aria-label="Search"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
