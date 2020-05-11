63ac01c578e2d20a40ec25fb450945de3efd0475
josiehpersson <josephine.h.persson@outlook.com>
2020-05-11 15:28:17 +0200

    ss

--- reactplatform/src/Komponenter/PopupFaktura/FakturaCard.js
+++ reactplatform/src/Komponenter/PopupFaktura/FakturaCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './PopupFaktura.css';
 
 function FakturaCard (props) {
     return(
         <div className="faktura-card">
-            
+            <p>{props.titel}: {props.referens}</p>
         </div>
     )
 }
