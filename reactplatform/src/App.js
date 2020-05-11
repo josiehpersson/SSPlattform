b0fd0acc4d08e13d61f8a5a42e1f81dc904ca7af
josiehpersson <josephine.h.persson@outlook.com>
2020-05-11 11:17:17 +0200

    Update App.js

--- reactplatform/src/App.js
+++ reactplatform/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-//import LoginBackground from './Pages/LoginPages/LoginBackground';
-import Konsultsida from './Pages/Konsultsida/Konsultsida';
-//import Konsultlåda from './Komponenter/KonsultLådor/Konsultlåda';
+import Login from './Pages/LoginPages/LogIn';
 
 function App() {
   return (
     <div className="App">
-      <Konsultsida />
+      <Login />
     </div>
   );
 }
