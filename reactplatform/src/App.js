b7a076a6be2cff7746025cbc79d24d9a5dab03ed
josiehpersson <josephine.h.persson@outlook.com>
2020-05-11 11:11:21 +0200

    företagssida klar

--- reactplatform/src/App.js
+++ reactplatform/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 //import LoginBackground from './Pages/LoginPages/LoginBackground';
-//import Konsultsida from './Pages/Konsultsida/Konsultsida';
-import Konsultlåda from './Komponenter/KonsultLådor/Konsultlåda';
-import Företagslåda from './Komponenter/FöretagsLådor/Företagslåda'
+import FöretagSida from './Pages/Företagssida/Företagssida';
 
 function App() {
   return (
     <div className="App">
-      <Företagslåda />
-      <Konsultlåda />
+      <FöretagSida />
     </div>
   );
 }
