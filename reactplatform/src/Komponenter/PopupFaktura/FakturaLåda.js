ef463649715c55845a1e0b14a2e569b78ba804bc
Josephine Halkola Persson <50661752+josiehpersson@users.noreply.github.com>
2020-05-12 11:39:53 +0200

    #66 (#81)
    * ww
    
    * ss
    
    * Popup-faktura klar
    
    * fakturalåda till popup
    
    * KLARRRR
    
    * prettier

---
+++ reactplatform/src/Komponenter/PopupFaktura/FakturaLåda.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './PopupFaktura.css';
+import FakturaBadge from './FakturaBadge';
+
+function FakturaLåda(props) {
+  return (
+    <div>
+      <table className="faktura-låda-container" onClick={props.onClick}>
+        <tr className="faktura-låda-top">
+          <th>Fakturanummer</th>
+          <th>Företag</th>
+          <th>Belopp</th>
+          <th>Förfallodatum</th>
+          <th>Status</th>
+        </tr>
+        <tr className="faktura-låda-bottom">
+          <td>{props.fakturanummer || '1234567'}</td>
+          <td>{props.företag || 'Namn'}</td>
+          <td>{props.belopp || '500SEK'}</td>
+          <td>{props.förfallodatum || '23/07/09'}</td>
+          <td>
+            <FakturaBadge />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default FakturaLåda;
--- reactplatform/src/Komponenter/UI-komponenter/ProfilBild.js
+++ reactplatform/src/Komponenter/UI-komponenter/ProfilBild.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React from "react";
+import './components.css';
+import { Container } from "reactstrap";
 
 function ProfilBild(props){
+    const imgstyle = {
+        width: 70,
+        height: 70,
+        backgroundColor: 'grey',
+        zIndex: 'inherit'
+    }
+
+    const containerstyle = {
+        top: props.top,
+        bottom: props.bottom,
+        left: props.left,
+        right: props.right,
+        zIndex: props.zIndex,
+    }
     
     return(
-       <div className="ProfilBild">
-           <img src={props.img} alt="Profile pic" className="Logotypeboll" />
+       <div style={containerstyle}>
+           <img src={props.img} alt="P" className="ProfilBild" style={imgstyle}/>
        </div>     
     )
 }
