66ef71be46351613ed80aaaafdf9dbc272ac6118
Josephine Halkola Persson <50661752+josiehpersson@users.noreply.github.com>
2020-05-14 10:50:56 +0200

    klart (#89)

---
+++ reactplatform/src/Pages/Tidrapportsida/Tidrapportsida.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Header from '../../Komponenter/Navbar/Header';
+import Tidrapplåda from '../Uppdragssida/Offert';
+import Boll from '../../Komponenter/Bollar/Boll';
+import SearchBar from '../../Komponenter/UI-komponenter/SearchBar';
+import FilterPopup from '../../Komponenter/Filter/FilterPopup';
+import './Tidrapportsida.css';
+
+function Tidrapportsida (props) {
+    return (
+      <div className="tidrapportsida-container">
+        <Header />
+        <div className="header-filler">
+          <Boll
+            className="grönboll"
+            height="180px"
+            width="180px"
+            top="70px"
+            left="100px"
+            zIndex="1"
+          />
+          <h3 className="tidrapport-page-header">TIDRAPPORTER</h3>
+          <Boll
+            className="gulboll"
+            height="50px"
+            width="50px"
+            top="210px"
+            left="210px"
+            zIndex="2"
+          />
+        </div>
+  
+        <div className="frostatGlas">
+          <div className="tidrapport-button-holder">
+            <FilterPopup />
+            <SearchBar />
+          </div>
+        <div className="tidrapport-låda-container">
+          <Tidrapplåda />
+          <Tidrapplåda />
+          <Tidrapplåda />
+          <Tidrapplåda />
+          <Tidrapplåda />
+          <Tidrapplåda />
+          <Tidrapplåda />
+        </div>
+        </div>
+      </div>
+    );
+  }
+  export default Tidrapportsida;
