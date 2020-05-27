fd5b11c35ef95b5e8f9d32918644e0f120393a02
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2020-05-27 11:41:43 +0200

    #107 (#113)
    * Update App.css
    
    * Update Datepicker.js
    
    * Update TidrappLåda.css
    
    * Update Tidrapportsida.css
    
    * Update Tidrapportsida.js
    
    * Update Detaljtidrappsida.js
    
    * Update Uppdragssida.js
    
    * Update Uppdragssida.css
    
    * Update Detaljtidrappsida.js
    
    * Update Tidrapportsida.css
    
    * Update Detaljtidrappsida.js

--- reactplatform/src/Pages/Tidrapportsida/Detaljtidrappsida.js
+++ reactplatform/src/Pages/Tidrapportsida/Detaljtidrappsida.js
@@ -56,18 +56,26 @@ class Detaljtidrapportsida extends React.Component {
               <li className="weekDay">Söndag</li>
             </ul>
         <div className="frostatGlas">
-          <div className="information-container">
-            <p>{this.state.name || 'Anna'}</p>
-            <p>{this.state.ID || '#3456'}</p>
-          </div>
-
           <div className="tidrapport-detalj-låda-container">
+            <ul className="weekDays">
+              <li className="weekDay">Måndag</li>
+              <li className="weekDay">Tisdag</li>
+              <li className="weekDay">Onsdag</li>
+              <li className="weekDay">Torsdag</li>
+              <li className="weekDay">Fredag</li>
+              <li className="weekDay">Lördag</li>
+              <li className="weekDay">Söndag</li>
+            </ul>
+        
+
+          
             <Tidrapplåda />
           </div>
         </div>
         <div className="tidrapport-detalj-button-holder">
           <PopupKommentar />
           <PrimaryButton text="GODKÄNN" />
+       
         </div>
         <MaskotBoll top="68%" right="1%" />
         <Boll className="grönboll" bottom="-40%" right="20%" />
